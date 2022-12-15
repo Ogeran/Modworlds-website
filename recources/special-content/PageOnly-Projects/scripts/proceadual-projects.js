b504b2fbe1e19ef01a4864d49cc6b90b4accab98
Ogeran <bennet@kulawik.de>
2022-12-15 14:29:56 +0100

    MWW_v1.7
    - Improved page headers.
    - Redesigned main page.
    - added projects page.
    - implemented procedural element generator.
    - Bug fixes.

---
+++ recources/special-content/PageOnly-Projects/scripts/proceadual-projects.js
@@ -0,0 +1,74 @@
+function UpdateProjects() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", './recources/special-content/PageOnly-Projects/scripts/projects.json', true);
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4) {
+            const projects = JSON.parse(xhr.responseText);
+            console.log(projects);
+
+            for(var i = 0; i < projects.Projects.length; i++) {
+                const project = projects.Projects[i];
+
+                const e = document.createElement("div");
+
+                e.style.width = '100%'
+                e.style.height = '20%'
+                e.style.backgroundImage = `url(${project.back})`;
+                e.style.backgroundSize = 'cover';
+                e.style.backgroundAttachment = 'fixed';
+                
+                e.style.display = 'flex';
+                e.style.flexWrap = 'nowrap'
+
+                e.style.marginTop = '25%';
+                e.style.marginBottom = '25%';
+
+                e.style.justifyContent = 'center';
+                e.style.alignItems = 'center';
+
+                const e_img = document.createElement("img");
+
+                e_img.src = project.PB;
+                e_img.style.borderRadius = '10000px';
+                e_img.style.width = '20%'
+                e_img.style.height = '20%';
+                e_img.style.margin = '2.5%'
+                e_img.style.aspectRatio = '1/1'
+
+                const e_textContainer = document.createElement("div");
+
+                e_textContainer.style.display = 'flex';
+                e_textContainer.style.flexWrap = 'wrap';
+                e_textContainer.style.height = '100%';
+                e_textContainer.style.width = '75%'
+
+                const e_title = document.createElement("a");
+
+                e_title.textContent = project.name;
+                e_title.style.color = '#ffffff';
+                e_title.style.fontSize = '7.5vw';
+                e_title.style.width = '90%';
+                e_title.style.height = '10%';
+                e_title.style.textAlign = 'center';
+                e_title.style.margin = '2.5%';
+
+                const e_description = document.createElement("a");
+
+                e_description.style.height = '75%'
+                e_description.style.width = '90%';
+                e_description.style.margin = '2.5%';
+                e_description.style.textAlign = 'center';
+                e_description.style.color = '#ffffff';
+                e_description.textContent = project.description;
+
+                e_textContainer.appendChild(e_title);
+                e_textContainer.appendChild(e_description);
+
+                e.appendChild(e_img);
+                e.appendChild(e_textContainer);
+                document.getElementById("back").appendChild(e);
+            }
+        }
+    }
+    xhr.send(null);
+}
--- recources/special-content/PageOnly-Projects/scripts/tabstyles.js
+++ recources/special-content/PageOnly-Projects/scripts/tabstyles.js
@@ -2,14 +2,10 @@ function setgraphics(tabnum) {
     if(document.body.offsetWidth > 900) {
         resetgraphicsPc();
         if(tabnum == 1) {
+            document.getElementById('TabTwo').style.borderBottomLeftRadius = '20px';
+        }
+        else if(tabnum == 2) {
             document.getElementById('TabOne').style.borderBottomRightRadius = '20px';
-            document.getElementById('TabThree').style.borderBottomLeftRadius = '20px';
-        }
-        else if(tabnum == 3) {
-            document.getElementById('TabFour').style.borderBottomLeftRadius = '20px';
-        }
-        else if(tabnum == 4) {
-            document.getElementById('TabThree').style.borderBottomRightRadius = '20px';
         }
     }
     else {
@@ -18,17 +14,17 @@ function setgraphics(tabnum) {
 }
 
 function resetgraphicsPc() {
+    document.getElementById('TabOne').style.borderTopRightRadius = '20px';
+
     document.getElementById('TabOne').style.borderBottomLeftRadius = '0px';
     document.getElementById('TabTwo').style.borderBottomLeftRadius = '0px';
-    document.getElementById('TabThree').style.borderBottomLeftRadius = '20px';
-    document.getElementById('TabFour').style.borderBottomLeftRadius = '0px';
+    document.getElementById('TabThree').style.borderBottomLeftRadius = '0px';
+    document.getElementById('TabFour').style.borderBottomLeftRadius = '20px';
 
-    document.getElementById('TabOne').style.borderBottomRightRadius = '20px';
-    document.getElementById('TabTwo').style.borderBottomRightRadius = '0px';
+    document.getElementById('TabOne').style.borderBottomRightRadius = '0px';
+    document.getElementById('TabTwo').style.borderBottomRightRadius = '20px';
     document.getElementById('TabThree').style.borderBottomRightRadius = '0px';
     document.getElementById('TabFour').style.borderBottomRightRadius = '0px';
-
-    document.getElementById('TabOne').style.borderTopRightRadius = '20px';
 }
 
 function resetgraphicsMobile() {
